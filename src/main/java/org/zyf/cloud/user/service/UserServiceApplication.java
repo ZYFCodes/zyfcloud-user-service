7bac33b497807c118fe89ea60a96f159f0b0eee6
zyfgit <1273740896@qq.com>
2019-06-16 23:26:16 +0800

    更新相关数据库操作

--- src/main/java/org/zyf/cloud/user/service/UserServiceApplication.java
+++ src/main/java/org/zyf/cloud/user/service/UserServiceApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -14,7 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 2019.06.02
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients
 @EnableDiscoveryClient
 @MapperScan("org.zyf.cloud.user.service.function.dao")
--- src/main/java/org/zyf/cloud/user/service/function/controller/RoleController.java
+++ src/main/java/org/zyf/cloud/user/service/function/controller/RoleController.java
@@ -2,9 +2,10 @@ package org.zyf.cloud.user.service.function.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.zyf.cloud.common.dto.RoleDto;
 import org.zyf.cloud.common.dto.UserDto;
@@ -23,10 +24,13 @@ import org.zyf.cloud.user.service.function.biz.RoleBiz;
 public class RoleController
 {
     @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    @Autowired
     private RoleBiz roleBiz;
 
     @RequestMapping(value = "/getRoleInfo/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public RoleDto getRole(@RequestParam("id") Integer id)
+    public RoleDto getRole(@PathVariable("id") Integer id)
     {
         return roleBiz.getRole(id);
     }
@@ -35,5 +39,11 @@ public class RoleController
     public UserDto getInfo()
     {
         return new UserDto();
+    }
+    
+    @RequestMapping(value = "/yy", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Long getInfoa()
+    {
+        return jdbcTemplate.queryForObject("select count(*) from zyfcloud_role", Long.class);
     }
 }
